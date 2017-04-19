a54aeb299691af14af7e1988640f5338259dd758
Sebastian Martin <acedia247@gmail.com>
2017-04-19 15:09:53 +0200

    implemented AJAX request to git API

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -27,24 +27,31 @@ SelectLanguage.PropTypes = {
 }
 
 function RepoGrid (props) {
-    return (
-        <ul className='popular-list'>
-            {props.repos.map(function(repo, index){
-                <li key={repo.name} class='popular-item'>
-                    <div className='popular-rank'>#{index + 1}</div>
-                    <ul className='space-list-items'>
-                        <li>
-                            <img
-                            className='avatar'
-                            src={repo.owner.avatar_url}
-                            alt={'Avatar for ' + repo.owner.login} />
-                        </li>
-                    </ul>
-                </li>
-            })}
-        </ul>
-    )
+  return (
+    <ul className='popular-list'>
+      {props.repos.map(function (repo, index) {
+        return (
+          <li key={repo.name} className='popular-item'>
+            <div className='popular-rank'>#{index + 1}</div>
+            <ul className='space-list-items'>
+              <li>
+                <img
+                  className='avatar'
+                  src={repo.owner.avatar_url}
+                  alt={'Avatar for ' + repo.owner.login}
+                />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
+
 
 class Popular extends React.Component{
     constructor (props){ //establish initial state, available through this.state
@@ -68,24 +75,28 @@ class Popular extends React.Component{
             }
         });
 
-        api.fetchPopularRepos(lang)
-        .then(function(repos){
-           this.setState(function(){
-               return {
-                repos: repos
-               }
-           })
-        }.bind(this));
+    api.fetchPopularRepos(lang)
+      .then(function (repos) {
+        this.setState(function () {
+          return {
+            repos: repos
+          }
+        });
+      }.bind(this));
+
     }
 
     render(){
         return(
+            
             <div>
                 <SelectLanguage
                 selectedLanguage={this.state.selectedLanguage}
                 onSelect={this.updateLanguage} /> 
-                <RepoGrid repos={this.state.repos} />
-                </div>
+                {!this.state.repos
+                ? <p>LOADING!</p>
+                : <RepoGrid repos={this.state.repos} />}
+            </div>
         )
     }
 }
