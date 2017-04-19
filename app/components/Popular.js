ba7cf8bb9cfbe2e502f214faf0a4604001153c5d
Sebastian Martin <acedia247@gmail.com>
2017-04-19 16:58:52 +0200

    changes layout

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -55,7 +55,7 @@ function RepoGrid (props) {
         return (
           <li key={repo.name} className='popular-item'>
             <div className='popular-rank'>#{index + 1}</div>
-            <ul className='space-list-items'>
+            <ul className='space-list-items'><a href={repo.html_url}>
               <li>
                 <img
                   className='avatar'
@@ -63,9 +63,9 @@ function RepoGrid (props) {
                   alt={'Avatar for ' + repo.owner.login}
                 />
               </li>
-              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>{repo.name}</li>
               <li>@{repo.owner.login}</li>
-              <li>{repo.stargazers_count} stars</li>
+              <li>{repo.stargazers_count} stars</li></a>
             </ul>
           </li>
         )
